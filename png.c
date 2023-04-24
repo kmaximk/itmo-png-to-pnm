bddd547455ad05abbd16f6c7c45f39b62f0726cd
Максим Колтаков <114956496+123herobrine@users.noreply.github.com>
2023-04-24 20:01:44 +0300

    добавил обработку фильтров

--- png.c
+++ png.c
@@ -1,13 +1,16 @@
-
+#if defined(ZLIB) + defined(LIBDEFLATE) + defined(ISAL) > 1
+#	error "Only one library can be defined"
+#elif defined(ZLIB)
+#	include <zlib.h>
+#elif defined(LIBDEFLATE)
+#	include <libdeflate.h>
+#elif defined(ISAL)
+#	include <isa-l/igzip_lib.h>
+#else
+#	error "Wrong library, use ZLIB or LIBDEFLATE or ISAL"
+#endif
 #include "return_codes.h"
 
-#if defined(ZLIB)
-#include <zlib.h>
-#elif defined(LIBDEFLATE)
-#include <libdeflate.h>
-#else
-#include <include/igzip_lib.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,13 +25,7 @@ struct image
 
 int inf(unsigned char *inputData, unsigned char *outputData, size_t inSize, size_t outSize)
 {
-#if defined(ZLIB) + defined(LIBDEFLATE) + defined(ISAL) > 1
-#	error "Only one library can be defined"
-#endif
-#if !defined(ZLIB) && !defined(LIBDEFLATE) && !defined(ISAL)
-#	error "Wrong library, use ZLIB or LIBDEFLATE or ISAL"
-#endif
-#ifdef ZLIB
+#if defined(ZLIB)
 	z_stream infl;
 	infl.zalloc = Z_NULL;
 	infl.zfree = Z_NULL;
@@ -43,35 +40,33 @@ int inf(unsigned char *inputData, unsigned char *outputData, size_t inSize, size
 		return ERROR_OUT_OF_MEMORY;
 	}
 	ret = inflate(&infl, Z_NO_FLUSH);
-	if (ret == Z_DATA_ERROR || ret == Z_STREAM_ERROR || ret == Z_BUF_ERROR)
-	{
-		return ERROR_DATA_INVALID;
-	}
-	else if (ret == Z_MEM_ERROR)
+	if (ret == Z_MEM_ERROR)
 	{
 		return ERROR_OUT_OF_MEMORY;
 	}
+	else if (ret != Z_OK)
+	{
+		return ERROR_DATA_INVALID
+	}
 	ret = inflateEnd(&infl);
-	if (ret == Z_STREAM_ERROR)
+	if (ret != Z_OK)
 	{
 		return ERROR_DATA_INVALID;
 	}
 	return SUCCESS;
-#endif
-#ifdef LIBDEFLATE
+#elif defined(LIBDEFLATE)
 	struct libdeflate_decompressor *de = libdeflate_alloc_decompressor();
 	if (de == NULL)
 	{
 		return ERROR_OUT_OF_MEMORY;
 	}
 	int res = libdeflate_deflate_decompress(de, inputData + 2, inSize - 6, outputData, outSize, NULL);
-	if (res == LIBDEFLATE_BAD_DATA || res == LIBDEFLATE_SHORT_OUTPUT || res == LIBDEFLATE_INSUFFICIENT_SPACE)
+	if (res != LIBDEFLATE_SUCCESS)
 	{
 		return ERROR_DATA_INVALID;
 	}
 	return SUCCESS;
-#endif
-#ifdef ISAL
+#elif defined(ISAL)
 	struct inflate_state infl;
 	isal_inflate_init(&infl);
 	infl.avail_in = inSize - 6;
@@ -190,9 +185,10 @@ void makeError(struct pair *ans, char *text, int ret)
 }
 struct pair parsePNG(FILE *f, struct image *buf)
 {
-	int ret;
+	size_t ret;
 	struct pair ans;
 	int plte = 1;
+	int idat = 1;
 	unsigned char tmp[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 	while (1)
 	{
@@ -217,8 +213,20 @@ struct pair parsePNG(FILE *f, struct image *buf)
 			return ans;
 		}
 		char name[8] = { tmp[4] & 0xFF, tmp[5] & 0xFF, tmp[6] & 0xFF, tmp[7] & 0xFF };
+		if (idat == 2 && strcmp(name, "IDAT") != 0)
+		{
+			idat = 0;
+		}
 		if (strcmp(name, "IDAT") == 0)
 		{
+			plte = 0;
+			if (idat == 0)
+			{
+				free(temp);
+				makeError(&ans, "IDAT chunks must be in consecutive order", ERROR_DATA_INVALID);
+				return ans;
+			}
+			idat = 2;
 			unsigned char *t = realloc((*buf).data, ((*buf).size + size) * sizeof(char));
 			if (t == NULL)
 			{
@@ -238,7 +246,6 @@ struct pair parsePNG(FILE *f, struct image *buf)
 			{
 				(*buf).data[(*buf).size + i] = temp[i];
 			}
-			plte = 0;
 			(*buf).size += size;
 		}
 		else if (strcmp(name, "IEND") == 0)
@@ -259,6 +266,7 @@ struct pair parsePNG(FILE *f, struct image *buf)
 				makeError(&ans, "Pallet chunk in wrong place\n", ERROR_DATA_INVALID);
 				return ans;
 			}
+			plte = 0;
 			(*buf).plteData = malloc(size);
 			(*buf).plteSize = size / 3;
 			if (!(*buf).plteData)
@@ -267,7 +275,6 @@ struct pair parsePNG(FILE *f, struct image *buf)
 				makeError(&ans, "Cannot alloc memory for pallet\n", ERROR_OUT_OF_MEMORY);
 				return ans;
 			}
-			plte = 0;
 			ret = fread((*buf).plteData, 1, size, f);
 			if (ret != size)
 			{
@@ -325,54 +332,95 @@ void writeToFile(FILE *f, unsigned char *out2, struct image buf, int asP5, int s
 		fwrite(out2, 1, size * type, f);
 	}
 }
-int convertRaw(int size, int type, int par[], unsigned char *out2, unsigned char *out1, struct image buf, int *asP5)
+unsigned char applyFilter(unsigned char filter, unsigned char out1, unsigned char *out2, int plte, int par[], int cnt, int x, int type)
 {
-	int x = 0;
-	int cnt = 0;
-	int i = 1;
-	while (i < size * type + par[1])
+	if (filter == 0)
 	{
-		if (buf.type == 3)
+		return out1;
+	}
+	else if (filter == 1)
+	{
+		if (cnt % par[0] != 0)
 		{
-			if (out1[i] > buf.plteSize)
+			return (out1 + out2[x - type]) & 0xFF;
+		}
+		return out1 & 0xFF;
+	}
+	else if (filter == 2)
+	{
+		if (cnt / par[0] != 0)
+		{
+			return (out1 + out2[x - (par[0] * type + plte)]) & 0xFF;
+		}
+		return out1;
+	}
+	else if (filter == 3)
+	{
+		int temp = (cnt % par[0] == 0 ? 0 : out2[x - type]) + (cnt / par[0] == 0 ? 0 : out2[x - (par[0] * type + plte)]);
+		temp /= 2;
+		return (out1 + temp) & 0xFF;
+	}
+	else if (filter == 4)
+	{
+		unsigned char a = (cnt % par[0] == 0 ? 0 : out2[x - type]) & 0xFF;
+		unsigned char b = (cnt / par[0] == 0 ? 0 : out2[x - (par[0] * type + plte)]) & 0xFF;
+		unsigned char c = (cnt % par[0] == 0 || cnt / par[0] == 0 ? 0 : out2[x - (par[0] * type + plte) - type]) & 0xFF;
+		int p = (a + b - c);
+		int ma = abs(p - a);
+		int mb = abs(p - b);
+		int mc = abs(p - c);
+		if (ma <= mb && ma <= mc)
+		{
+			return (out1 + a) & 0xFF;
+		}
+		else if (mb <= mc)
+		{
+			return (out1 + b) & 0xFF;
+		}
+		else
+		{
+			return (out1 + c) & 0xFF;
+		}
+	}
+	return 0;
+}
+int palletRaw(int size, int type, int par[], unsigned char *out2, unsigned char *out1, struct image buf, int *asP5)
+{
+	unsigned char filter;
+	int cnt = 0;
+	int x = 0;
+	for (int j = 0; j < par[1]; j++)
+	{
+		filter = out1[j * (par[0] * type + 1)];
+		if (filter < 0 || filter > 4)
+		{
+			return -1;
+		}
+		for (int k = 1; k < par[0] * type + 1; k++)
+		{
+			out1[j * (par[0] * type + 1) + k] =
+				applyFilter(filter, out1[j * (par[0] * type + 1) + k], out1, 1, par, cnt, j * (par[0] * type + 1) + k, type);
+			cnt++;
+			if (out1[j * (par[0] * type + 1) + k] > buf.plteSize)
 			{
 				return -2;
 			}
-			for (int j = 0; j < 3; j++)
+			for (int l = 0; l < 3; l++)
 			{
-				out2[x + j] = buf.plteData[3 * out1[i] + j];
+				out2[x + l] = buf.plteData[out1[j * (par[0] * type + 1) + k] * 3 + l];
 			}
 			if (!isGrayScale(out2[x], out2[x + 1], out2[x + 2]))
 			{
 				*asP5 = 0;
 			}
 			x += 3;
-			i += 1;
-		}
-		else
-		{
-			for (int j = 0; j < type; j++)
-			{
-				out2[x + j] = out1[i + j];
-			}
-			x += type;
-			i += type;
-		}
-		cnt++;
-		if (cnt % par[0] == 0)
-		{
-			if (i < size * type + par[1] && (out1[i] != 0))
-			{
-				return -1;
-			}
-			i++;
 		}
 	}
 	if (*asP5 == 1 && buf.type == 3)
 	{
 		cnt = 0;
 		x = 0;
-		i = 1;
+		int i = 1;
 		while (i < size * type + par[1])
 		{
 			out2[x] = buf.plteData[3 * out1[i]];
@@ -387,9 +435,40 @@ int convertRaw(int size, int type, int par[], unsigned char *out2, unsigned char
 	}
 	return 0;
 }
+int convertRaw(int size, int type, int par[], unsigned char *out2, unsigned char *out1, struct image buf, int *asP5)
+{
+	int x = 0;
+	int cnt = 0;
+	int i = 1;
+	unsigned char filter = out1[0];
+	if (buf.type == 3)
+	{
+		return palletRaw(size, type, par, out2, out1, buf, asP5);
+	}
+	while (i < size * type + par[1])
+	{
+		for (int j = 0; j < type; j++)
+		{
+			out2[x + j] = applyFilter(filter, out1[i + j], out2, 0, par, cnt, x + j, type);
+		}
+		x += type;
+		i += type;
+		cnt++;
+		if (cnt % par[0] == 0)
+		{
+			if (i < size * type + par[1] && (out1[i] < 0 || out1[i] > 4))
+			{
+				return -1;
+			}
+			filter = out1[i];
+			i++;
+		}
+	}
+	return 0;
+}
 void checkFree(unsigned char *f)
 {
-	if (f)
+	if (f != NULL)
 	{
 		free(f);
 	}
@@ -415,7 +494,6 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Cannot open input file\n");
 		return ERROR_CANNOT_OPEN_FILE;
 	}
-	double n = 0;
 	int ret = fread(buf.data, 1, size, f);
 	if (ret != size)
 	{
@@ -449,6 +527,13 @@ int main(int argc, char *argv[])
 	buf.data = NULL;
 	buf.size = 0;
 	struct pair r = parsePNG(f, &buf);
+	if (buf.size == 0)
+	{
+		fclose(f);
+		checkFree(buf.plteData);
+		fprintf(stderr, "No IDAT chunks found\n");
+		return ERROR_DATA_INVALID;
+	}
 	if (r.returnCode != SUCCESS)
 	{
 		fclose(f);
@@ -497,15 +582,15 @@ int main(int argc, char *argv[])
 	ret = convertRaw(size, type, par, out2, out1, buf, &asP5);
 	if (ret != 0)
 	{
-		free(out1);
-		free(out2);
+		checkFree(out1);
+		checkFree(out2);
 		checkFree(buf.plteData);
 		if (ret == -1)
 		{
 			fprintf(stderr, "Unsupported filter, only support filter None\n");
 			return ERROR_UNSUPPORTED;
 		}
-		else if (ret == -2)
+		else
 		{
 			fprintf(stderr, "Pallet index greater than its size\n");
 			return ERROR_DATA_INVALID;
@@ -522,7 +607,8 @@ int main(int argc, char *argv[])
 	}
 	writeToFile(f, out2, buf, asP5, size, type, par);
 	fclose(f);
-	free(out1);
-	free(out2);
+	checkFree(out1);
+	checkFree(out2);
 	checkFree(buf.plteData);
+	return SUCCESS;
 }
